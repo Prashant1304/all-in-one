be166cb282368f9378e6d4c8e9367a74840bcf09
Prashant1304 <sinha.prashant134@gmail.com>
2021-09-09 00:41:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -12,40 +12,53 @@ function App() {
   return (
     <div className="App">
         <div>
-            <img className="images" src={tinder}></img>
-            <a href="https://tinder-apppp.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Tinder </a>
+            <h2 className="header">Please Click on Title Name below Image to open the specified project</h2>
+            <h3 className="header">Every single project is compatable with both android and desktop view</h3>
+            <p className="exception">Except This One</p>
         </div>
 
-        <div>
-            <img className="images" src={parking}></img>   
-            <a href="https://parking-lot-appp.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Parking Lot </a>
-        </div>
+        <div className="main-container">
+            <div className="container">
+                <div className="projects">
+                    <img className="images" src={tinder}></img>
+                    <a className="name" href="https://tinder-apppp.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Tinder </a>
+                </div>
 
-        <div>   
-            <img className="images" src={bank}></img>
-            <a href="https://bank-appp.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Banking App </a>
-        </div>
+                <div className="projects">
+                    <img className="images" src={parking}></img>   
+                    <a className="name" href="https://parking-lot-appp.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Parking Lot </a>
+                </div>
 
-        <div>   
-            <img className="images" src={calcultor}></img>
-            <a href="https://react-calculato.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Android Calculator </a>
-        </div>
+                <div className="projects">   
+                    <img className="images" src={bank}></img>
+                    <a className="name" href="https://bank-appp.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Banking App </a>
+                </div>
+            </div>
 
-        <div>
-            <img className="images" src={quiz}></img>   
-            <a href="https://r-quiz.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Quiz </a>
-        </div>
+            <div className="container">
+                <div className="projects">   
+                    <img className="images" src={calcultor}></img>
+                    <a className="name" href="https://react-calculato.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Android Calculator </a>
+                </div>
 
-        <div>
-            <img className="images" src={toDoList}></img>
-            <a href="https://r-to-do-list.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> To Do List </a>
-        </div>
+                <div className="projects">
+                    <img className="images" src={quiz}></img>   
+                    <a className="name" href="https://r-quiz.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Quiz </a>
+                </div>
 
-        <div>
-            <img className="images" src={tipCalculator}></img>
-            <a href="https://tip-calculatorr.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Tip Calculator </a>
-        </div>
+                <div className="projects">
+                    <img className="images" src={toDoList}></img>
+                    <a className="name" href="https://r-to-do-list.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> To Do List </a>
+                </div>
+            </div>
 
+            <div className="container">
+                <div className="projects">
+                    <img className="images" src={tipCalculator}></img>
+                    <a className="name" href="https://tip-calculatorr.herokuapp.com/%20deployed%20to%20Heroku" target="_blank"> Tip Calculator </a>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
